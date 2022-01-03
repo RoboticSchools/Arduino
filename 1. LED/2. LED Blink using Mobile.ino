ef4213b6c83b14f8d8f0b1803a1a0619bca368ae
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-01-03 17:40:09 +0530

    Update 2. LED Blink using Mobile.ino

--- 1. LED/2. LED Blink using Mobile.ino
+++ 1. LED/2. LED Blink using Mobile.ino
@@ -1,1 +1,28 @@
 
+#include<SoftwareSerial.h>
+SoftwareSerial myserial(2,3);
+int led =9;
+void setup()
+{
+  myserial.begin(9600);
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  if(myserial.available()>0)
+  {
+    int data = myserial.read();
+    if(data==1)
+    {
+      digitalWrite(led,HIGH);  
+    }
+    else if(data==0)
+    {
+     digitalWrite(led,LOW);
+    }
+    
+  }
+  
+}
