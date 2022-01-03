4a1081f9f6c7309cd767c1f2d7b3c17200ba9a54
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-01-03 17:46:26 +0530

    Update 2. Mixing 2 Colors.ino

--- 2. RGB LED/2. Mixing 2 Colors.ino
+++ 2. RGB LED/2. Mixing 2 Colors.ino
@@ -1,1 +1,33 @@
 
+int red = 11;
+int green = 10;
+int blue = 9;
+
+void setup()
+{
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(red, HIGH);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+  delay(2000); 
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, HIGH);
+  delay(2000);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, HIGH);
+  delay(2000);
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
+  delay(2000);
+
+}
