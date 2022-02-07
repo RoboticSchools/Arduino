b278642fa5cb58e8bd75f1e6304e4eee7671f898
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-02-07 09:50:05 +0530

    Update 1. LED Blink using LDR Sensor.ino

--- 4. Light Following Robot/1. LED Blink using LDR Sensor.ino
+++ 4. Light Following Robot/1. LED Blink using LDR Sensor.ino
@@ -11,12 +11,12 @@ void setup()
 void loop()
 {
   int sensor = digitalRead(ldr);
-  if (sensor == 0)
+  if (sensor == 1)
   {
     digitalWrite(led,HIGH);
     Serial.println("It's Dark... LED ON");
   }
-  else if (sensor == 1)
+  else if (sensor == 0)
   {
     digitalWrite(led,LOW);
     Serial.println("It's SunnyDay... LED OFF");
