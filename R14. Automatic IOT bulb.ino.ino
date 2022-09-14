461979d198067aff088d5a4f42ad56e69cda423d
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-09-14 18:06:23 +0530

    Create R14. Automatic IOT bulb.ino.ino

---
+++ R14. Automatic IOT bulb.ino.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+int sensor = 0;
+
+void setup()
+{
+  pinMode(7, INPUT);
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  sensor = digitalRead(7);
+  Serial.println(sensor);
+  if (sensor == 0) {
+    digitalWrite(8, HIGH);
+  } else {
+    digitalWrite(8, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
