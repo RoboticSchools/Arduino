36db19f13947d44a3f98498e4a420eaeebbe3e61
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-09-14 18:11:29 +0530

    Update and rename 14a. Mobile control IOT Bulb.ino to 14.1. Mobile control IOT Bulb.ino

---
+++ 14.1. Mobile control IOT Bulb.ino
@@ -0,0 +1,26 @@
+//www.roboticschools.com
+
+#include<SoftwareSerial.h>
+
+SoftwareSerial blue;
+
+void setup()
+{
+  pinMode(8,OUTPUT);
+  blue.begin(9600);
+}
+
+void loop()
+{
+  if(blue.available()>0)
+  {
+    int data = blue.read();
+    if(data==1)                // To turn ON LED
+    {
+      digitalWrite(8,HIGH);
+    }
+    if(data==0)                // To turn OFF LED
+    {
+      digitalWrite(8,LOW);
+    }
+  }
