a93e2bda04dc8be28c1cda932f99b7f20ac7b4ab
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-09-14 17:20:41 +0530

    Create 7. Mobile Control LED.ino

---
+++ 7. Mobile Control LED.ino
@@ -0,0 +1,30 @@
+//www.roboticschools.com
+
+#include<SoftwareSerial.h>
+
+SoftwareSerial blue;
+
+void setup()
+{
+  pinMode(9,OUTPUT);
+  blue.begin(9600);
+}
+
+void loop()
+{
+  if(blue.available()>0)
+  {
+    int data = blue.read();
+    if(data==1)                // To turn ON LED
+    {
+      digitalWrite(9,HIGH);
+    }
+    if(data==0)                // To turn OFF LED
+    {
+      digitalWrite(9,LOW);
+    }
+    if(data>2)                 // Brightness Control
+    {
+      analogWrite(9,data);
+    }
+  }
