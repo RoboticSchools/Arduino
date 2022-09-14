471ab1d55a19d8c014f02e13d5233047bea97d8e
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-09-14 17:18:11 +0530

    Create 5. RGB Colour switcher.ino

---
+++ 5. RGB Colour switcher.ino
@@ -0,0 +1,22 @@
+// www.roboticschools.com
+
+int button = 0;
+
+void setup()
+{
+  pinMode(3, INPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  button = digitalRead(3);
+  if (button == 1) {
+    analogWrite(11, random(0, 255 + 1));
+    analogWrite(9, random(0, 255 + 1));
+    analogWrite(10, random(0, 255 + 1));
+    delay(300); // Wait for 300 millisecond(s)
+  }
+}
