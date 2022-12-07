da57483aab040e677e654a4469a7990217559bd3
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-12-07 22:19:29 +0530

    Update R20. Display names and numbers on LCD.ino

--- R20. Display names and numbers on LCD.ino
+++ R20. Display names and numbers on LCD.ino
@@ -8,10 +8,8 @@ void setup()
 {
 	// initialize the LCD
 	lcd.begin();
-
-	// Turn on the blacklight and print a message.
-	lcd.backlight();
-	lcd.print("Hello, world!");
+	lcd.clear();
+	lcd.print("RoboticSchools");
 }
 
 void loop()
