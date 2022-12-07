541334245495fe637f907aa032e7faaa992284d5
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2022-12-07 22:29:43 +0530

    Update R18. Smart Dustbin.ino

--- R18. Smart Dustbin.ino
+++ R18. Smart Dustbin.ino
@@ -1,37 +1,37 @@
-//www.roboticschools.com
+// www.roboticschools.com
 
 #include <Servo.h>
 
-int ultrasonic = 0;
+Servo motor;
+int triggerPin = 7;
+int echoPin = 8;
 
-long readUltrasonicDistance(int triggerPin, int echoPin)
+void setup()
 {
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  motor.attach(3, 500, 2500);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+void loop()
+{
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
   // Sets the trigger pin to HIGH state for 10 microseconds
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
   // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
-}
-
-Servo servo_3;
-
-void setup()
-{
-  servo_3.attach(3, 500, 2500);
-}
-
-void loop()
-{
-  ultrasonic = 0.01723 * readUltrasonicDistance(9, 8);
-  if (ultrasonic < 20) {
-    servo_3.write(150);
-  } else {
-    servo_3.write(0);
+  long duration =  pulseIn(echoPin, HIGH);
+  int distance = 0.0343 * duration/2;
+  
+  if (distance < 20) 
+  {
+    motor.write(150);
+  } 
+  else 
+  {
+    motor.write(0);
   }
   delay(10); // Delay a little bit to improve simulation performance
 }
