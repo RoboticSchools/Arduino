1502838ff052f4df6420897e7520e754b11a513b
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2023-01-30 16:36:33 +0530

    Update R24. Keypad Calculator.ino

--- R24. Keypad Calculator.ino
+++ R24. Keypad Calculator.ino
@@ -1,105 +1,66 @@
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
-
+#include <Adafruit_LiquidCrystal.h>
+#include <Wire.h>
 #include <Keypad.h>
+
+Adafruit_LiquidCrystal lcd(0);
 
 const byte ROWS = 4;
 const byte COLS = 4;
-
-char keys [ROWS] [COLS] = {
+char keys[ROWS][COLS] = {
   {'1', '2', '3', '+'},
   {'4', '5', '6', '-'},
   {'7', '8', '9', '*'},
-  {'C', '0', '=', '/'}
+  {'c', '0', '=', '/'}
 };
+
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3, 2};
 
-Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
+String inputString = "";
+float result = 0;
+char operation = 0;
 
-boolean presentValue = false;
-boolean next = false;
-boolean final = false;
-String num1, num2;
-int answer = 0;
-char op;
-
-void setup()
-{
-  lcd.begin();                      
-  lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("RoboticSchools");
-  lcd.setCursor(3,1);
-  lcd.print("Calculator");
-  delay(3000);
+void setup() {
+  lcd.begin(16,2);
   lcd.clear();
 }
-
 void loop() {
-  char key = myKeypad.getKey();
-
-  if (key != NO_KEY && (key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9' || key == '0'))
-  {
-    if (presentValue != true)
-    {
-      num1 = num1 + key;
-      int numLength = num1.length();
-      lcd.setCursor(0, 0); 
-      lcd.print(num1);
-    }
-    else
-    {
-      num2 = num2 + key;
-      int numLength = num2.length();
-      int numLength1 = num1.length();
-      lcd.setCursor(1 + numLength1, 0);
-      lcd.print(num2);
-      final = true;
-    }
+  char key = keypad.getKey();
+  if (key) {
+    if (key >= '0' && key <= '9') {
+      inputString += key;
+      lcd.print(key);
+    } else if (key == '+' || key == '-' || key == '*' || key == '/') {
+      calculate();
+      operation = key;
+      lcd.print(key);
+    } else if (key == '=') {
+      calculate();
+      lcd.print(key);
+      lcd.print(result);
+    } else if (key == 'c') {
+      inputString = "";
+      result = 0;
+      operation = 0;
+      lcd.clear();
+    } 
   }
+}
 
-  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+'))
-  {
-    if (presentValue == false)
-    {
-      int numLength = num1.length();
-      presentValue = true;
-      op = key;
-      lcd.setCursor(0 + numLength, 0);
-      lcd.print(op);
-    }
-  }
-
-  else if (final == true && key != NO_KEY && key == '=') {
-      
-    if (op == '+') {
-      answer = num1.toInt() + num2.toInt();
-    }
-    else if (op == '-') {
-      answer = num1.toInt() - num2.toInt();
-    }
-    else if (op == '*') {
-      answer = num1.toInt() * num2.toInt();
-    }
-    else if (op == '/') {
-      answer = num1.toInt() / num2.toInt();
-    }
-    lcd.clear();
-    lcd.setCursor(16, 1);
-    lcd.autoscroll();
-    lcd.print(answer);
-    lcd.noAutoscroll();
-    
-  }
-  else if (key != NO_KEY && key == 'C') {
-    lcd.clear();
-    presentValue = false;
-    final = false;
-    num1 = "";
-    num2 = "";
-    answer = 0;
-    op = ' ';
+void calculate() {
+  float input = inputString.toFloat();
+  inputString = "";
+  if (operation == '+') {
+    result += input;
+  } else if (operation == '-') {
+    result -= input;
+  } else if (operation == '*') {
+    result *= input;
+  } else if (operation == '/') {
+    result /= input;
+  } else {
+    result = input;
   }
 }
