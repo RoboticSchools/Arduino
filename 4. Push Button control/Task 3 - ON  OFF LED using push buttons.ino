815d91ecdabbd3b5097f5ccbba900c8a890f7082
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2023-05-03 13:50:09 +0530

    Update Task 3 - ON  OFF LED using push buttons.ino

--- 4. Push Button control/Task 3 - ON  OFF LED using push buttons.ino
+++ 4. Push Button control/Task 3 - ON  OFF LED using push buttons.ino
@@ -1,17 +1,20 @@
-const int buttonPin = 2;
+const int buttonPin1 = 2;
+const int buttonPin2 = 3;
 const int ledPin = 9;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
+  int buttonState1 = digitalRead(buttonPin1);
+  int buttonState2 = digitalRead(buttonPin2);
   
-  if (buttonState == LOW) {
+  if (buttonState1 == LOW) {
     digitalWrite(ledPin, HIGH);
-  } else {
+  } else if (buttonState2 == LOW) {
     digitalWrite(ledPin, LOW);
   }
 }
