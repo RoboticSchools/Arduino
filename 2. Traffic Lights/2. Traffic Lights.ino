23c0ee412948c2804029ed93f33af9140be0b931
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2023-05-03 12:53:53 +0530

    Update 2. Traffic Lights.ino

--- 2. Traffic Lights/2. Traffic Lights.ino
+++ 2. Traffic Lights/2. Traffic Lights.ino
@@ -1,26 +1,35 @@
-const int redPin = 9;
-const int yellowPin = 10;
-const int greenPin = 11;
+int redLedPin = 11;    // Define the red LED pin number
+int yellowLedPin = 10; // Define the yellow LED pin number
+int greenLedPin = 9;   // Define the green LED pin number
 
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(yellowPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);     // Set the red LED pin as output
+  pinMode(yellowLedPin, OUTPUT);  // Set the yellow LED pin as output
+  pinMode(greenLedPin, OUTPUT);   // Set the green LED pin as output
 }
 
 void loop() {
-  // Red light on
-  digitalWrite(redPin, HIGH);
-  delay(3000);
-  digitalWrite(redPin, LOW);
+  // Red light on, green and yellow off
+  digitalWrite(redLedPin, HIGH);
+  digitalWrite(yellowLedPin, LOW);
+  digitalWrite(greenLedPin, LOW);
+  delay(5000);
 
-  // Green light on
-  digitalWrite(greenPin, HIGH);
-  delay(3000);
-  digitalWrite(greenPin, LOW);
+  // Red and yellow light on, green off
+  digitalWrite(redLedPin, HIGH);
+  digitalWrite(yellowLedPin, HIGH);
+  digitalWrite(greenLedPin, LOW);
+  delay(2000);
 
-  // Yellow light on
-  digitalWrite(yellowPin, HIGH);
-  delay(1000);
-  digitalWrite(yellowPin, LOW);
+  // Green light on, red and yellow off
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(yellowLedPin, LOW);
+  digitalWrite(greenLedPin, HIGH);
+  delay(5000);
+
+  // Yellow light on, red and green off
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(yellowLedPin, HIGH);
+  digitalWrite(greenLedPin, LOW);
+  delay(2000);
 }
