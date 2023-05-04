a81b7b02d2617b1ac0bd1662a13610e7e4616994
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2023-05-04 20:25:24 +0530

    Update 9. Mini Table Fan.ino

--- 9.  Mini Table Fan/9. Mini Table Fan.ino
+++ 9.  Mini Table Fan/9. Mini Table Fan.ino
@@ -1,1 +1,21 @@
-ok
+// Define the pins for the motor
+int motorPin1 = 8;
+int motorPin2 = 9;
+
+void setup() {
+  // Set the motor pins as output
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+}
+
+void loop() {
+  // Turn the motor clockwise (forward) for 2 seconds
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  delay(2000);
+
+  // Turn the motor counterclockwise (backward) for 2 seconds
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  delay(2000);
+}
