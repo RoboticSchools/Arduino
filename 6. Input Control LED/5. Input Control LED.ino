43aee844210b73c71942f7b0240069cab21af890
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2023-05-04 11:21:41 +0530

    Update 5. Input Control LED.ino

--- 6. Input Control LED/5. Input Control LED.ino
+++ 6. Input Control LED/5. Input Control LED.ino
@@ -1,20 +1,20 @@
-int ledPin = 13; // Define the LED pin as 13
+int ledPin = 13;
+char command;
 
 void setup() {
-  Serial.begin(9600); // Start serial communication at 9600 baud rate
-  pinMode(ledPin, OUTPUT); // Set LED pin as output
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0) { // If there's data available in serial buffer
-    char input = Serial.read(); // Read the incoming data
-    if (input == '1') { // If received data is '1'
-      digitalWrite(ledPin, HIGH); // Turn on the LED
-      Serial.println("LED is on"); // Print message on serial monitor
-    }
-    else if (input == '0') { // If received data is '0'
-      digitalWrite(ledPin, LOW); // Turn off the LED
-      Serial.println("LED is off"); // Print message on serial monitor
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED ON");
+    } else if (command == '0') {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED OFF");
     }
   }
 }
